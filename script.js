88449ef0a7a3e5f272296588f7fa4359fe8ee15d
HamO001 <ndarugastiv9@gmail.com>
2024-06-01 17:39:44 +0000

    Add task management functionality using localStorage

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function addTask(){
-    if(inputBox.value === ''){
-        alert("please enter a task!");
-    }
-    else{
+function addTask() {
+    if (inputBox.value === '') {
+        alert("Please enter a task!");
+    } else {
         let li = document.createElement('li');
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
+        li.textContent = inputBox.value;
+
+        // Create a delete button
         let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
+        span.textContent = "\u00d7";
         li.appendChild(span);
+
+        listContainer.appendChild(li);
+        inputBox.value = "";
+
+        saveData();
     }
-    inputBox.value = "";
-    saveData()   
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData()
-    }
-    else if(e.target.tagName === "SPAN"){
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
-        saveData()
+        saveData();
     }
-}, false);
+});
 
-function saveData(){
+function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
-function showTask(){
+
+function showTask() {
     listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
